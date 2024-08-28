8a9e01223d7fde53f3154765d2072683e2d27931
Abhijit Kumbhar <abhijitkumbhar33@.com>
2024-08-28 12:35:18 +0530

    new message

--- server/config.js
+++ server/config.js
@@ -2,5 +2,6 @@ module.exports ={
     JWT_SECRET :"abhijitkumbhar",
     EMAIL : "margie.nader29@ethereal.email",
     PASSWORD :"FjWFsQDYZ9CjGwXZTS",
-    ATLAS_URL : "mongodb+srv://admin12:admin123@cluster0.pbw7u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    ATLAS_URL : "mongodb+srv://admin12:admin123@cluster0.pbw7u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    PORT : "https://login-with-otp-api.vercel.app"
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const { errors } = require('mongodb-memory-server');
+// const { errors } = require('mongodb-memory-server');
 const connect = require('./database/conn.js')
-const router = require('./router/route.js')
+const router = require('./router/route.js');
+const { PORT } = require('./config.js');
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use(cors({
 app.use(morgan('tiny'));
 app.disable('x-powered-by') //less hackers know our stack
 
-const port = 8080;
+const port = PORT;
 
 app.get('/', (req, res) => {
     res.status(201).json("Home GET Request")
