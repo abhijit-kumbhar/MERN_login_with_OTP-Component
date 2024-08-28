66d167dc392ccfd9eb9f2402c7988399a184051a
Abhijit Kumbhar <abhijitkumbhar33@.com>
2024-08-28 16:36:07 +0530

    new message 2

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import {jwtDecode} from 'jwt-decode'
 
-axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
+// axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
+axios.defaults.baseURL = "https://login-with-otp-api.vercel.app";
 axios.defaults.withCredentials = true;
 
 /*Make api request*/
