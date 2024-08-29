cbed9cf0bb74b6d87733001a7a67f790ec031d65
Abhijit Kumbhar <abhijitkumbhar33@.com>
2024-08-29 12:11:01 +0530

    new message 5

--- server/index.js
+++ server/index.js
@@ -4,21 +4,21 @@ const morgan = require('morgan');
 // const { errors } = require('mongodb-memory-server');
 const connect = require('./database/conn.js')
 const router = require('./router/route.js');
-const { PORT } = require('./config.js');
+const ENV = require('./config.js');
 
 const app = express();
 
 /*middleware*/
 app.use(express.json());
 app.use(cors({
-    origin: "https://login-with-otp-dusky.vercel.app",
+    origin: "*",
     methods : ["POST", "GET", "PUT", "DELETE"],
     credentials:true
 }));
 app.use(morgan('tiny'));
 app.disable('x-powered-by') //less hackers know our stack
 
-const port = 8080;
+const port =8080 || ENV.PORT;
 
 app.get('/', (req, res) => {
     res.status(201).json("Home GET Request")
