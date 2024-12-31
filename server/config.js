70a9d9e991e3753ee3bfd49e9d6d140412a209a7
Abhijit Kumbhar <120697855+abhijit-kumbhar@users.noreply.github.com>
2024-12-31 18:25:59 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -2,6 +2,6 @@ module.exports ={
     JWT_SECRET :"abhijitkumbhar",
     EMAIL : "margie.nader29@ethereal.email",
     PASSWORD :"FjWFsQDYZ9CjGwXZTS",
-    ATLAS_URL : "mongodb+srv://admin12:<db_password>@cluster0.pbw7u.mongodb.net/test.users",
+    ATLAS_URL : "mongodb+srv://admin12:abhijitkumbhar@cluster0.pbw7u.mongodb.net/test/users",
     PORT : "https://login-with-otp-dusky.vercel.app"
 }
